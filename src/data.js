c4ce1fae2464480d28bf2d1b022661d3c0569082
saltowl <vladislava-shakirova@mail.ru>
2019-08-29 14:20:09 +0300

    Add third set of sounds

--- src/data.js
+++ src/data.js
@@ -8,7 +8,8 @@ function CreatePadData(keyName, name) {
 
 export const KEYS_NUMBER = 9;
 const allSounds = ['Bass Drum 1', 'Bass Drum 2', 'Bass Drum 3', 'Boom Kick', 'Deep Kick', 'E-Mu Proteus FX 909 Kick', 'E-Mu Proteus FX Wacky Kick', 'Electronic Kick 1', 'Electronic Kick 2',
-'1980s Casio Piano C5', 'Alesis Fusion Bright Acoustic Piano C6', 'Casio MT-45 Piano C4', 'E-Mu Proteus FX Pianotar C3', 'Ensoniq ESQ-1 Piano C3', 'Alesis Fusion Nylon String Guitar C4', 'Ensoniq SQ-1 ClassicGuitar C5', 'Kawai K5000W NylonGt1 C4', 'Kawai K5000W SteelGt1 C3'];
+'1980s Casio Piano C5', 'Alesis Fusion Bright Acoustic Piano C6', 'Casio MT-45 Piano C4', 'E-Mu Proteus FX Pianotar C3', 'Ensoniq ESQ-1 Piano C3', 'Alesis Fusion Nylon String Guitar C4', 'Ensoniq SQ-1 ClassicGuitar C5', 'Kawai K5000W NylonGt1 C4', 'Kawai K5000W SteelGt1 C3',
+'Crash Cymbal 1', 'Crash Cymbal 5', 'Closed Hi-Hat 2', 'Ensoniq SQ-1 Open Hi-Hat', 'Ensoniq SQ-1 Ride Cymbal', 'Claves', 'Alesis Fusion Recorder C5', 'Electro Tom', 'Alesis Fusion English Horn C5'];
 const keyNames = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
 const padData = [];
 for (let i = 0; i < allSounds.length / KEYS_NUMBER; i++) {
