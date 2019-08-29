90e9445274dc0c8bcb19151cc9b9ea7e01138afb
saltowl <vladislava-shakirova@mail.ru>
2019-08-29 14:26:08 +0300

    Updates

---
+++ precache-manifest.581c205d7eb644d79cec03d563efd4c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48501400bd36624bff5259fde240f951",
+    "url": "/DrumMachine/index.html"
+  },
+  {
+    "revision": "f9764308d79e68a361a7",
+    "url": "/DrumMachine/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b17df5f9bc200df09431",
+    "url": "/DrumMachine/static/css/main.e430b42c.chunk.css"
+  },
+  {
+    "revision": "f9764308d79e68a361a7",
+    "url": "/DrumMachine/static/js/2.fc8f342e.chunk.js"
+  },
+  {
+    "revision": "b17df5f9bc200df09431",
+    "url": "/DrumMachine/static/js/main.fe8a1a45.chunk.js"
+  },
+  {
+    "revision": "e5d750ac85b10868dd38",
+    "url": "/DrumMachine/static/js/runtime~main.afb0728b.js"
+  }
+]);
